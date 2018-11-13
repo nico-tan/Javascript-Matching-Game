db589285aaf06114ec53d2fa226b9e9ffbae909c
Nico Tan <n.tan@hotmail.ca>
2018-11-12 23:07:05 -0800

    Display and Click Functions Defined
    Click functions are defined, bug with getting html dom type attribute

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ let cards = [...card];
 const deck = document.getElementById("card-deck");
 
 // Declare matched cards
-let matched = document.getElementsByClassName("match");
+let matchedCard = document.getElementsByClassName("match");
 
 // Declare array for matched cards
 var opened = [];
@@ -42,3 +42,79 @@ function startGame() {
     cards[i].classList.remove("show", "open", "match", "disabled");
   }
 }
+
+// Toggles open show class to display cards
+var displayCard = function() {
+  // console.log("Card Pressed");
+  this.classList.toggle("open");
+  this.classList.toggle("show");
+  // This disables the card making it unclickable until it's closed
+  this.classList.toggle("disabled");
+  console.log("Card flipped, successfully disabled");
+};
+
+// Add opened cards to the opened list and check whether there's a match
+function cardOpen() {
+  opened.push(this);
+  console.log(this);
+  var len = opened.length;
+  var type = opened[len-1].type;
+  // console.log("opened has " + len + " cards inside");
+  console.log("Object " + len + " has type of " + type);
+  if(len === 2){
+    console.log("Second card clicked, checking for match");
+    if(opened[0].type === opened[1].type){
+      console.log("Tiles Matched");
+      // matched();
+    } else {
+      console.log("Tiles didn't match");
+      // unmatched();
+    }
+  }
+};
+
+// Function for when cards match
+function matched() {
+  opened[0].classList.add("match", "disabled");
+  opened[1].classList.add("match", "disabled");
+  opened[0].classList.remove("show", "open", "no-event");
+  opened[1].classList.remove("show", "open", "no-event");
+  opened = [];
+}
+
+// Function for when cards don't match
+function unmatched() {
+  opened[0].classList.add("unmatched");
+  opened[1].classList.add("unmatched");
+  disable();
+  setTimeout(function() {
+    opened[0].classList.remove("show", "open", "no-event", "unmatched");
+    opened[1].classList.remove("show", "open", "no-event", "unmatched");
+    enable();
+    opened = [];
+  }, 1100);
+}
+
+// disable cards temporarily
+function disable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.add('disabled');
+    });
+}
+
+// enable cards and disable matched cards
+function enable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.remove('disabled');
+        for(var i = 0; i < matchedCard.length; i++){
+            matchedCard[i].classList.add("disabled");
+        }
+    });
+}
+
+// Add event handler for each card element
+for(var i = 0; i < cards.length; i++){
+  card = cards[i];
+  card.addEventListener("click", displayCard);
+  card.addEventListener("click", cardOpen);
+};
