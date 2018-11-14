31d74f4868669d6027cdd6a43f4fc6526a64aae4
Nico Tan <n.tan@hotmail.ca>
2018-11-14 06:50:44 -0800

    Basic Functionality Completed
    Added all working functions

--- js/game.js
+++ js/game.js
@@ -8,6 +8,13 @@ const deck = document.getElementById("card-deck");
 // Declare matched cards
 let matchedCard = document.getElementsByClassName("match");
 
+// Declare variable for moves made
+let moves = 0;
+let counter = document.querySelector(".moves");
+
+// Declare variables for endgame stats
+
+
 // Declare array for matched cards
 var opened = [];
 
@@ -41,34 +48,47 @@ function startGame() {
 
     cards[i].classList.remove("show", "open", "match", "disabled");
   }
+
+  // Reset moves
+  moves = 0;
+  counter.innerHTML = moves;
+
+  // Reset counter
+  second = 0;
+  minute = 0;
+  hour = 0;
+  var timer = document.querySelector(".timer");
+  timer.innerHTML = "0 mins 0 secs";
+  clearInterval(interval);
 }
 
 // Toggles open show class to display cards
 var displayCard = function() {
   // console.log("Card Pressed");
   this.classList.toggle("open");
-  this.classList.toggle("show");
+  // this.classList.toggle("show");
   // This disables the card making it unclickable until it's closed
   this.classList.toggle("disabled");
-  console.log("Card flipped, successfully disabled");
+  // console.log("Card flipped, successfully disabled");
 };
 
 // Add opened cards to the opened list and check whether there's a match
 function cardOpen() {
   opened.push(this);
-  console.log(this);
+  console.log(this.dataset.name);
   var len = opened.length;
-  var type = opened[len-1].type;
-  // console.log("opened has " + len + " cards inside");
-  console.log("Object " + len + " has type of " + type);
   if(len === 2){
+    moveCounter();
     console.log("Second card clicked, checking for match");
-    if(opened[0].type === opened[1].type){
+    if(opened[0].dataset.name === opened[1].dataset.name){
       console.log("Tiles Matched");
+      // setTimeout(matched(), 6200);
       // matched();
+      setTimeout(match(), 5000);
+      // match();
     } else {
       console.log("Tiles didn't match");
-      // unmatched();
+      unmatched();
     }
   }
 };
@@ -77,9 +97,28 @@ function cardOpen() {
 function matched() {
   opened[0].classList.add("match", "disabled");
   opened[1].classList.add("match", "disabled");
-  opened[0].classList.remove("show", "open", "no-event");
-  opened[1].classList.remove("show", "open", "no-event");
+  disable();
+  setTimeout(function() {
+    opened[0].classList.remove("show", "open", "no-event");
+    opened[1].classList.remove("show", "open", "no-event");
+    enable();
+    opened = [];
+  }, 1100
+
+);
+  // opened[0].classList.remove("show", "open", "no-event");
+  // opened[1].classList.remove("show", "open", "no-event");
+  // opened = [];
+}
+
+function match() {
+  opened[0].classList.add("match");
+  opened[1].classList.add("match");
   opened = [];
+}
+
+function resetMatch() {
+
 }
 
 // Function for when cards don't match
@@ -112,9 +151,56 @@ function enable(){
     });
 }
 
+// function to count player's moves
+function moveCounter() {
+  moves++;
+  console.log("Move #" + moves);
+  counter.innerHTML = moves;
+
+  // Once the first move is made, start the clock
+  if(moves == 1) {
+    second = 0;
+    minute = 0;
+    hour = 0;
+    startTimer();
+  }
+}
+
+// function to keep track of game time
+var second = 0, minute = 0, hour = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer() {
+  interval = setInterval(function() {
+    timer.innerHTML = minute + "mins " + second + "secs";
+    second++;
+    if(second == 60) {
+      minute++;
+      second = 0;
+    }
+
+    if(minute == 60) {
+      hour++;
+      minute = 0;
+    }
+  }, 1000);
+}
+
+// function to determine whether the win condition is met or not
+function congratulations() {
+  if(matchedCard.length == 16){
+    clearInterval(interval);
+    finalTime = timer.innerHTML;
+
+    console.log("It took you: " + finalTime + " to finish");
+    console.log("It took you " + moves + " moves");
+  };
+}
+
 // Add event handler for each card element
 for(var i = 0; i < cards.length; i++){
   card = cards[i];
   card.addEventListener("click", displayCard);
   card.addEventListener("click", cardOpen);
+  card.addEventListener("click", congratulations);
 };
